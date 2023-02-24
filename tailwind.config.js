69dd9a859e067ca7144e85dedb37f64688015cc5
Sofía González <71847414+SofiaShaba@users.noreply.github.com>
2023-02-23 20:44:02 -0600

    Configuración de tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './src/**/*.{html,ts}',
+  ],
+  darkMode: 'class',
+  theme: {
+    fontFamily: {
+      mukta: 'Mukta',
+    },
+  },
+  variants: {},
+  plugins: [],
+};
